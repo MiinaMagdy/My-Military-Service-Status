56fd97ae3dd73d086b814606d83e8dbcaedf333e
Mina Magdy <mina.magdy.takawey@gmail.com>
2026-03-17 14:41:16 +0200

    Add time off entry for March 2026

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -87,6 +87,10 @@ export class AppComponent implements OnInit {
       from: new Date('2026-02-25 00:00:00'),
       to: new Date('2026-03-02 00:00:00'),
     }),
+    new TimeOff({
+      from: new Date('2026-03-17 00:00:00'),
+      to: new Date('2026-03-20 00:00:00'),
+    })
   ];
 
   ngOnInit(): void {
